8e192f68e1858d13d619781d2557cf59d76e70ec
shreya-sridhar <shreyasridhar1109@gmail.com>
2021-01-26 17:10:54 -0800

    done

--- project_frontend/index.js
+++ project_frontend/index.js
@@ -325,70 +325,70 @@ function setCircleDasharray() {
 
 
 
-// const canvasEl = document.querySelector('#canvas');
+const canvasEl = document.querySelector('#canvas');
 
-// const w = canvasEl.width = window.innerWidth;
-// const h = canvasEl.height = window.innerHeight * 2;
+const w = canvasEl.width = window.innerWidth;
+const h = canvasEl.height = window.innerHeight * 2;
 
-// function loop() {
-//   requestAnimationFrame(loop);
-// 	ctx.clearRect(0,0,w,h);
+function loop() {
+  requestAnimationFrame(loop);
+	ctx.clearRect(0,0,w,h);
   
-//   confs.forEach((conf) => {
-//     conf.update();
-//     conf.draw();
-//   })
-// }
+  confs.forEach((conf) => {
+    conf.update();
+    conf.draw();
+  })
+}
 
-// function Confetti () {
-//   //construct confetti
-//   const colours = ['#fde132', '#009bde', '#ff6b00'];
+function Confetti () {
+  //construct confetti
+  const colours = ['#fde132', '#009bde', '#ff6b00'];
   
-//   this.x = Math.round(Math.random() * w);
-//   this.y = Math.round(Math.random() * h)-(h/2);
-//   this.rotation = Math.random()*360;
+  this.x = Math.round(Math.random() * w);
+  this.y = Math.round(Math.random() * h)-(h/2);
+  this.rotation = Math.random()*360;
 
-//   const size = Math.random()*(w/60);
-//   this.size = size < 15 ? 15 : size;
+  const size = Math.random()*(w/60);
+  this.size = size < 15 ? 15 : size;
 
-//   this.color = colours[Math.floor(colours.length * Math.random())];
+  this.color = colours[Math.floor(colours.length * Math.random())];
 
-//   this.speed = this.size/2;
+  this.speed = this.size/2;
   
-//   this.opacity = Math.random();
+  this.opacity = Math.random();
 
-//   this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
-// }
+  this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
+}
 
-// Confetti.prototype.border = function() {
-//   if (this.y >= h) {
-//     this.y = h;
-//   }
-// }
+Confetti.prototype.border = function() {
+  if (this.y >= h) {
+    this.y = h;
+  }
+}
 
-// Confetti.prototype.update = function() {
-//   this.y += this.speed;
+Confetti.prototype.update = function() {
+  this.y += this.speed;
   
-//   if (this.y <= h) {
-//     this.x += this.shiftDirection/3;
-//     this.rotation += this.shiftDirection*this.speed/100;
-//   }
+  if (this.y <= h) {
+    this.x += this.shiftDirection/3;
+    this.rotation += this.shiftDirection*this.speed/100;
+  }
 
-//   if (this.y > h) this.border();
-// };
+  if (this.y > h) this.border();
+};
 
-// Confetti.prototype.draw = function() {
-//   ctx.beginPath();
-//   ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
-//   ctx.lineTo(this.x, this.y);
-//   ctx.closePath();
-//   ctx.globalAlpha = this.opacity;
-//   ctx.fillStyle = this.color;
-//   ctx.fill();
-// };
+Confetti.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
+  ctx.lineTo(this.x, this.y);
+  ctx.closePath();
+  ctx.globalAlpha = this.opacity;
+  ctx.fillStyle = this.color;
+  ctx.fill();
+};
 
-// const ctx = canvasEl.getContext('2d');
-// const confNum = Math.floor(w / 4);
-// const confs = new Array(confNum).fill().map(_ => new Confetti());
+const ctx = canvasEl.getContext('2d');
+const confNum = Math.floor(w / 4);
+const confs = new Array(confNum).fill().map(_ => new Confetti());
 
-// loop();
+loop();
